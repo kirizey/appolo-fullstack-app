568cc019961b3f1ece41cc16a3ecb20976d3f886
Kirill Dolgikh <kirill.dolgikh@saritasa.com>
2020-01-27 08:45:49 +0700

    Complete server

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -63,7 +63,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
